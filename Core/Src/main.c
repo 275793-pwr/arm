3bf362ad74bf471e3134e85cd1a0d77af76a9ea1
= <275793@student.pwr.edu.pl>
2025-03-20 13:30:06 +0100

    Blinking in circle

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -117,16 +117,22 @@ int main(void)
   while (1)
   {
 
-	  HAL_Delay(250);
-	  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-	  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
-	  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-	  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-	  HAL_Delay(250);
-	  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-	  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
-	  HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-	  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+	  for (int i = 0; i < 4; ++i) {
+
+		  HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+		  HAL_Delay(250);
+		  HAL_GPIO_TogglePin(LD4_GPIO_Port, LD4_Pin);
+		  HAL_Delay(250);
+		  HAL_GPIO_TogglePin(LD5_GPIO_Port, LD5_Pin);
+		  HAL_Delay(250);
+		  if (i > 1)
+		  {
+			  HAL_GPIO_TogglePin(LD6_GPIO_Port, LD6_Pin);
+			  HAL_Delay(250);
+
+		  }
+	}
+
 
     /* USER CODE END WHILE */
     MX_USB_HOST_Process();
